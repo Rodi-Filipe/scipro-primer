624b1ac5816641711335beb1e9e2480df435bf73
Rodi-Filipe <rodi.filipe17@gmail.com>
2021-09-30 20:42:35 +0100

    'Updates'

--- class/dYdt.py
+++ class/dYdt.py
@@ -24,28 +24,24 @@ class Derivative(object):
         f, h = self.f, self.h # make short forms 
         return (f(x+h) - f(x))/h
 
-from Y import Y
-
-from Derivative import Derivative
-
 y1=Y(5)
 
 D1=Derivative(y1)
 
 print(D1(0))
--25.819072981647647
+'-25.819072981647647'
 
 print(5-9.81*(0))
--25.819023931715872
+'-25.819023931715872'
 
 print(D1(0.5*5/9.81))
-2.4999509500078787
+'2.4999509500078787'
 
 print(5-9.81*(0.5*5/9.81))
-2.5
+'2.5'
 
 print(D1(5/9.81))
--4.905000849930729e-05
+'-4.905000849930729e-05'
 
 print(5-9.81*(5/9.81))
-0.0
+'0.0'
